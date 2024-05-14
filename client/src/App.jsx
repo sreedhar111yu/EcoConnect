4f5f1400c9be7179bcd10ec94934e4b545158c76
sreedhar111yu <sreedharv36@gmail.com>
2024-05-14 19:32:00 +0530

    logo added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div>App</div>
+   <>
+   <BrowserRouter>
+   <Navbar/>
+
+   
+   </BrowserRouter>
+   
+   </>
   )
 }
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:"#f42c37",
+        secondary:"#f42c37",
+        brandYellow:"#fdc62e",
+        brandGreen:"#2dcc6f",
+        brandBlue:"#1376f4",
+        brandWhite:"#eeeeee"
+      },
+      container:{
+        center:true,
+        padding:{
+          DEFAULT:"1rem",
+          sm:"3rem",
+          // lg:'4rem',
+          // xl:'5rem',
+          
+        }
+      }
+    },
+
   },
   plugins: [],
 }
